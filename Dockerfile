7e0220b86ca935363fd3df1355b73646f03b76ca
Karel Bemelmans <karel.bemelmans@nordcloud.se>
2016-10-29 17:47:18 +0200

    Re-write of Dockerfile to make this image actually usable with mounted volumes

--- Dockerfile
+++ Dockerfile
@@ -1,49 +1,35 @@
-FROM unibet/base-debian-git-jre8:latest
+FROM unibet/alpine-jre:7
 MAINTAINER karel.bemelmans@unibet.com
 
-# build time environment variables
-ENV GO_VERSION=16.10.0-4131 \
-    USER_NAME=go \
-    USER_ID=999 \
-    GROUP_NAME=go \
-    GROUP_ID=999
+# Install more apk packages we might need
+RUN apk --update add curl
 
-# install dependencies
-RUN echo "deb http://http.debian.net/debian jessie-backports main" | tee /etc/apt/sources.list.d/jessie-backports.list \
-    && apt-get update \
-    && apt-get install -y \
-        apache2-utils \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+# Add go user and group
+RUN addgroup go && adduser -h /var/lib/go-server -H -S -G go go
 
-# install go server
-RUN groupadd -r -g $GROUP_ID $GROUP_NAME \
-    && useradd -r -g $GROUP_NAME -u $USER_ID -d /var/go $USER_NAME \
-    && curl -fSL "https://download.go.cd/binaries/${GO_VERSION}/deb/go-server_${GO_VERSION}_all.deb" -o go-server.deb \
-    && dpkg -i go-server.deb \
-    && rm -rf go-server.db \
-    && sed -i -e "s/DAEMON=Y/DAEMON=N/" /etc/default/go-server \
-    && mkdir -p /var/lib/go-server/plugins/external \
-    && curl -fSL "https://github.com/gocd-contrib/gocd-oauth-login/releases/download/v1.2/google-oauth-login-1.2.jar" -o /var/lib/go-server/plugins/external/google-oauth-login-1.2.jar \
-    && curl -fSL "https://github.com/ashwanthkumar/gocd-slack-build-notifier/releases/download/v1.4.0-RC6/gocd-slack-notifier-1.4.0-RC6.jar" -o /var/lib/go-server/plugins/external/gocd-slack-notifier-1.4.0-RC6.jar
+# Install GoCD Server from zip file
+ARG GO_MAJOR_VERSION=16.11.0
+ARG GO_BUILD_VERSION=4185
+ARG GO_VERSION="${GO_MAJOR_VERSION}-${GO_BUILD_VERSION}"
+ARG GOCD_SHA256=c8fa2dc52dd4797d8f2aa85823f8896dc4d89ee77b3b23925c93afd885080875
 
-# runtime environment variables
-ENV AGENT_KEY="" \
-    GC_LOG="" \
-    JVM_DEBUG="" \
-    SERVER_MAX_MEM=1024m \
-    SERVER_MAX_PERM_GEN=256m \
-    SERVER_MEM=512m \
-    SERVER_MIN_PERM_GEN=128m \
-    GO_SERVER_PORT=8153 \
-    GO_SERVER_SSL_PORT=8154 \
-    USER_AUTH=""
+RUN curl -L --silent https://download.go.cd/binaries/${GO_VERSION}/generic/go-server-${GO_VERSION}.zip \
+       -o /tmp/go-server.zip \
+  && echo "${GOCD_SHA256}  /tmp/go-server.zip" | sha256sum -c - \
+  && unzip /tmp/go-server.zip -d /usr/local \
+  && ln -s /usr/local/go-server-${GO_MAJOR_VERSION} /usr/local/go-server \
+  && chown -R go:go /usr/local/go-server-${GO_MAJOR_VERSION} \
+  && rm /tmp/go-server.zip
 
-# expose ports
+# Expose ports needed
 EXPOSE 8153 8154
 
+# These are the 3 volumes we define
+# You should mount these as external mount points from the Docker host
+RUN mkdir -p /etc/go /var/lib/go-server /var/log/go-server
+VOLUME ['/var/lib/go-server', '/var/log/go-server', '/etc/go']
+
+# add the entrypoint config and run it when we start the container
 COPY ./docker-entrypoint.sh /
-
 RUN chmod 500 /docker-entrypoint.sh
-
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# set user and group
-groupmod -g ${GROUP_ID} ${GROUP_NAME}
-usermod -g ${GROUP_ID} -u ${USER_ID} ${USER_NAME}
-
-# log to std out instead of file
+# We overwrite this file everytime we run the container, so all your local changes
+# will be lost. If you need to change the logging, fork this repo or build your
+# own Docker image on top of this.
+#
+# Log to stdout instead of files
 cat >/etc/go/log4j.properties <<EOL
 log4j.rootLogger=WARN, ConsoleAppender
 log4j.logger.com.thoughtworks.go=INFO
@@ -28,141 +28,35 @@ log4j.appender.ShineConsoleAppender.layout=org.apache.log4j.PatternLayout
 log4j.appender.ShineConsoleAppender.layout.conversionPattern=%d{ISO8601} %5p [%t] %c{1}:%L - %m%n
 EOL
 
-# chown directories that might have been mounted as volume and thus still have root as owner
+# Fix directory permissions if needed
+#
+# /var/lib/go-server
 if [ -d "/var/lib/go-server" ]
 then
   echo "Setting owner for /var/lib/go-server..."
-  chown ${USER_NAME}:${GROUP_NAME} /var/lib/go-server
+  chown go:go /var/lib/go-server
 else
   echo "Directory /var/lib/go-server does not exist"
 fi
 
-if [ -d "/var/lib/go-server/artifacts" ]
-then
-  echo "Setting owner for /var/lib/go-server/artifacts..."
-  chown ${USER_NAME}:${GROUP_NAME} /var/lib/go-server/artifacts
-else
-  echo "Directory /var/lib/go-server/artifacts does not exist"
-fi
-
-if [ -d "/var/lib/go-server/db" ]
-then
-  echo "Setting owner for /var/lib/go-server/db..."
-  chown -R ${USER_NAME}:${GROUP_NAME} /var/lib/go-server/db
-else
-  echo "Directory /var/lib/go-server/db does not exist"
-fi
-
-if [ -d "/var/lib/go-server/plugins" ]
-then
-  echo "Setting owner for /var/lib/go-server/plugins..."
-  chown -R ${USER_NAME}:${GROUP_NAME} /var/lib/go-server/plugins
-else
-  echo "Directory /var/lib/go-server/plugins does not exist"
-fi
-
+# /var/log/go-server
 if [ -d "/var/log/go-server" ]
 then
   echo "Setting owner for /var/log/go-server..."
-  chown -R ${USER_NAME}:${GROUP_NAME} /var/log/go-server
+  chown -R go:go /var/log/go-server
 else
   echo "Directory /var/log/go-server does not exist"
 fi
 
+# /etc/go
 if [ -d "/etc/go" ]
 then
   echo "Setting owner for /etc/go..."
-  chown -R ${USER_NAME}:${GROUP_NAME} /etc/go
+  chown -R go:go /etc/go
 else
   echo "Directory /etc/go does not exist"
 fi
 
-if [ -d "/k8s-ssh-secret" ]
-then
-
-  echo "Copying files from /k8s-ssh-secret to /var/go/.ssh"
-  mkdir -p /var/go/.ssh
-  cp -Lr /k8s-ssh-secret/* /var/go/.ssh
-
-else
-  echo "Directory /k8s-ssh-secret does not exist"
-fi
-
-if [ -d "/var/go" ]
-then
-  echo "Setting owner for /var/go..."
-  chown -R ${USER_NAME}:${GROUP_NAME} /var/go || echo "No write permissions"
-else
-  echo "Directory /var/go does not exist"
-fi
-
-if [ -d "/var/go/.ssh" ]
-then
-
-  # make sure ssh keys mounted from kubernetes secret have correct permissions
-  echo "Setting owner for /var/go/.ssh..."
-  chmod 400 /var/go/.ssh/* || echo "Could not write permissions for /var/go/.ssh/*"
-
-  # rename ssh keys to deal with kubernetes secret name restrictions
-  cd /var/go/.ssh
-  for f in *-*
-  do
-    echo "Renaming $f to ${f//-/_}..."
-    mv "$f" "${f//-/_}" || echo "No write permissions for /var/go/.ssh"
-  done
-
-  ls -latr /var/go/.ssh
-
-else
-  echo "Directory /var/go/.ssh does not exist"
-fi
-
-if [ "${USER_AUTH}" != "" ]
-then
-  echo "Creating htpasswd file at location /etc/gocd-auth"
-  touch /etc/gocd-auth
-
-  for auth in $USER_AUTH
-  do
-    values=$(echo $auth | tr ":" "\n")
-    user=$(echo "$values" | head -n1)
-    pass=$(echo "$values" | tail -n1)
-    htpasswd -sb /etc/gocd-auth $user $pass
-    echo "User \"${user}\" created"
-  done
-fi
-
-# update config to point to set the internal ports
-sed -i -e "s/GO_SERVER_PORT=8153/GO_SERVER_PORT=${GO_SERVER_PORT}/" /etc/default/go-server
-sed -i -e "s/GO_SERVER_SSL_PORT=8154/GO_SERVER_SSL_PORT=${GO_SERVER_SSL_PORT}/" /etc/default/go-server
-
 # start go.cd server as go user
 echo "Starting go.cd server..."
-/bin/su - ${USER_NAME} -c "GC_LOG=$GC_LOG JVM_DEBUG=$JVM_DEBUG SERVER_MEM=$SERVER_MEM SERVER_MAX_MEM=$SERVER_MAX_MEM SERVER_MIN_PERM_GEN=$SERVER_MIN_PERM_GEN SERVER_MAX_PERM_GEN=$SERVER_MAX_PERM_GEN GO_NOTIFY_CONF=$GO_NOTIFY_CONF /usr/share/go-server/server.sh" &
-
-supid=$!
-
-echo "Go.cd server pid: $supid"
-
-# wait until server is up and running
-echo "Waiting for go.cd server to be ready..."
-until curl -s -o /dev/null 'http://localhost:8153'
-do
-  sleep 1
-done
-
-echo "Go.cd server is ready"
-
-# set agent key in cruise-config.xml
-if [ -n "$AGENT_KEY" ]
-then
-  echo "Setting agent key..."
-  sed -i -e 's/agentAutoRegisterKey="[^"]*" *//' -e 's#\(<server\)\(.*artifactsdir.*\)#\1 agentAutoRegisterKey="'$AGENT_KEY'"\2#' /etc/go/cruise-config.xml
-fi
-
-# wait for /bin/su process, so container fails if server fails
-wait $supid
-
-echo "Go.cd server stopped"
-ps
-0
+/usr/local/go-server/server.sh
