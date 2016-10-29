c434228e205a182a4c56b022fd6937051a3ef0d9
Karel Bemelmans <karel.bemelmans@nordcloud.se>
2016-10-29 22:01:52 +0200

    Update agentAutoRegisterKey in cruise-config.xml during server launch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,6 +30,22 @@ log4j.appender.ShineConsoleAppender.layout.conversionPattern=%d{ISO8601} %5p [%t
 EOL
 fi
 
+# Server config
+#
+# Update it with the AGENT_KEY if needed, otherwise create a new clean config.
+if [ -f go-data/etc/cruise-config.xml ]; then
+  if [ -z $AGENT_KEY ]; then
+    xmlstarlet ed -u /cruise/server/@agentAutoRegisterKey -v ${AGENT_KEY} go-data/etc/cruise-config.xml
+  fi
+else
+cat >/etc/go/cruise-config.xml <<EOL
+<?xml version="1.0" encoding="utf-8"?>
+<cruise xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="cruise-config.xsd" schemaVersion="87">
+  <server agentAutoRegisterKey="${AGENT_KEY}"/>
+</cruise>
+EOL
+fi
+
 # start go.cd server as go user
 echo "Starting go.cd server..."
 /usr/local/go-server/server.sh
